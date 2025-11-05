fcb1c4cd71e9b04c3728240b9ecccaa6a614be75
Kunal khuteta <khutetakunal@gmail.com>
2025-11-05 09:49:50 +0530

    render deployment

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
   timeout: 20000,
 });
